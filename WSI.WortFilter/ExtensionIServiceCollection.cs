8aea1b377a4877750fc8458e4ce4aa8b3c645c45
Bernhard Ninaus <ninausbernhard@gmail.com>
2021-11-29 15:03:03 +0100

    WortFilter IServiceCollection verbessert.

--- WSI.WortFilter/ExtensionIServiceCollection.cs
+++ WSI.WortFilter/ExtensionIServiceCollection.cs
@@ -1,26 +1,58 @@
 using Microsoft.Extensions.DependencyInjection;
 namespace WSI.WortFilter;
 
-public static class ExtensionIServiceCollection {
-    public static IServiceCollection AddWortFilterBasisc(this IServiceCollection services) {
-        #region Wort Austauscher
-        services.AddSingleton<SternchenWortAustauschService>();
+public class WortSucheServiceCollection {
+    private readonly IServiceCollection _services;
 
-        services.AddSingleton<IWortAustauschService>(provider 
-            => provider.GetRequiredService<SternchenWortAustauschService>());
+    public WortSucheServiceCollection(IServiceCollection services) {
+        _services = services;
 
-        services.AddSingleton<LeerWortAustauschService>();
+        #region Wort Austausche
+        _services.AddSingleton<SternchenWortAustauschService>();
+        _services.AddSingleton<LeerWortAustauschService>();
         #endregion
-
 
         #region Wort Sucher
         services.AddSingleton<EinfacheWortSucheService>(provider 
             => new EinfacheWortSucheService(StringComparison.CurrentCultureIgnoreCase));
-
-        services.AddSingleton<IWortSucheService>(provider 
-            => provider.GetRequiredService<EinfacheWortSucheService>());
         #endregion
-
-        return services;
     }
+
+    public WortSucheServiceCollection AddDefautlWortAustasuch() {
+        _services.AddSingleton<IWortAustauschService>(provider 
+            => provider.GetRequiredService<SternchenWortAustauschService>());
+
+        return this;
+    }
+
+    public WortSucheServiceCollection AddDefaultWortSuche() {
+        _services.AddSingleton<IWortSucheService>(provider 
+            => provider.GetRequiredService<EinfacheWortSucheService>());
+
+        return this;
+    }
+
+    public WortSucheServiceCollection AddEinfacherWortFilter<T1, T2>(string[] worte)
+            where T1 : IWortAustauschService
+            where T2 : IWortSucheService {
+        _services.AddSingleton<IWortFilterService>(provider 
+            => new EinfacherWortFilterService(
+                worte,
+                provider.GetRequiredService<T1>(),
+                provider.GetRequiredService<T2>()));
+
+        return this;
+    }
+
+    public WortSucheServiceCollection AddEinfacherWortFilter<T>(string[] worte)
+            where T : IWortAustauschService
+        => AddEinfacherWortFilter<T, IWortSucheService>(worte);
+
+    public WortSucheServiceCollection AddEinfacherWortFilter(string[] worte)
+        => AddEinfacherWortFilter<IWortAustauschService, IWortSucheService>(worte);
+}
+
+public static class ExtensionIServiceCollection {
+    public static WortSucheServiceCollection AddWortFilter(this IServiceCollection services)
+        => new WortSucheServiceCollection(services);
 }
