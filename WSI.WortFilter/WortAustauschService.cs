6279e9cf7c9abd205556e19394c0a4b6d493ba44
Bernhard Ninaus <ninausbernhard@gmail.com>
2021-11-30 16:12:43 +0100

    SternchenWortAustauschService verallgemeint

--- WSI.WortFilter/WortAustauschService.cs
+++ WSI.WortFilter/WortAustauschService.cs
@@ -1,7 +1,18 @@
+using System.Data;
+using System.Diagnostics.Contracts;
 namespace WSI.WortFilter;
 
-public class SternchenWortAustauschService : IWortAustauschService {
-    public string TauscheWort(string zuTauschen) => new String('*', zuTauschen.Length);
+public class EinfacherWortAustaschService : IWortAustauschService {
+    private readonly char _ziechen;
+    public EinfacherWortAustaschService(char ziechen) {
+        _ziechen = ziechen;
+    }
+    
+    public string TauscheWort(string zuTauschen) => new String(_ziechen, zuTauschen.Length);
+}
+
+public class SternchenWortAustauschService : EinfacherWortAustaschService {
+    public SternchenWortAustauschService() : base('*') { }
 }
 
 public class LeerWortAustauschService : IWortAustauschService {
